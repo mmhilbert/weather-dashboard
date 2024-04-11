6164fa9e654868ac8e19f2fa2c9ac8180268e26a
Megan Hilbert <meganmhilbert@gmail.com>
2024-04-11 18:02:40 -0500

    completed weather app functionality, added card styling to css.

--- assets/scripts.js
+++ assets/scripts.js
@@ -8,6 +8,7 @@ function getCityWeather(cityName) {
         console.log(response)
         if (response.ok) {
             // add city to local storage in an array
+            addCityToLocalStorage(cityName)
             return response.json()
         } else {
             console.log('there was an error')
@@ -16,13 +17,32 @@ function getCityWeather(cityName) {
     })
     .then(function(weatherData) {
         console.log(weatherData)
-        // populate cityInfo (sub row1)
+        // TODO: populate cityInfo (sub row1)
+        renderCityCard(weatherData)
+
+
         // call getFiveDayForecast function + pass lat + long as parameters
         getFiveDayForecast(weatherData.coord.lat, weatherData.coord.lon)
     })
     .catch(function(error) {
         alert(error.message)
     })
+}
+
+function renderCityCard(weatherData) {
+    const cityName = weatherData.name
+    const cityTemp = weatherData.main.temp
+    const cityWind = weatherData.wind.speed
+    const cityHumidity = weatherData.main.humidity
+
+    const cityForecastCard = `<div class="col-12">
+        <h3 id="city-name">City: ${cityName}</h3>
+        <p id="city-temp">Temperature: ${cityTemp} °F</p>
+        <p id="city-wind">Wind: ${cityWind} MPH</p>
+        <p id="city-humidity">Humidity: ${cityHumidity}%</p>
+    </div>`
+
+    $('#day-weather').empty().append(cityForecastCard)
 }
 
 function getFiveDayForecast(lat, lon) {
@@ -39,7 +59,13 @@ function getFiveDayForecast(lat, lon) {
         }
     })
     .then(function(weatherForecastData) {
-        console.log(weatherForecastData)
+        renderFiveDayForecast(weatherForecastData)
+        
+    })
+}
+
+function renderFiveDayForecast(weatherForecastData) {
+    console.log(weatherForecastData)
         // get 0, 8, 16, 24, 32 in the array on the api call
         const day1 = weatherForecastData.list[0]
         const day2 = weatherForecastData.list[8]
@@ -55,12 +81,56 @@ function getFiveDayForecast(lat, lon) {
         const weatherCard4 = createWeatherForecastCard(day4)
         const weatherCard5 = createWeatherForecastCard(day5)
 
+        $('.forecast-card').remove()
+
         $('#weather-forecast').append(weatherCard1)
         .append(weatherCard2)
         .append(weatherCard3)
         .append(weatherCard4)
         .append(weatherCard5)
-        
+}
+
+function getCityListFromLS() {
+    let cityNameArray = localStorage.getItem('cityName')
+
+    if (cityNameArray == null) {
+        cityNameArray = []
+    } else {
+        cityNameArray = JSON.parse(cityNameArray)
+    }
+    return cityNameArray
+}
+
+function addCityToLocalStorage(cityName) {
+    let cityNameArray = getCityListFromLS()
+
+    cityNameArray.unshift(cityName)
+    localStorage.setItem('cityName', JSON.stringify(cityNameArray))
+    renderSearchHistory()
+}
+
+function createCityListCard(city) {
+    const cityCard = 
+    `<div class="card search-history-card" data-city="${city}">
+        <div class="card-body">
+         ${city}
+        </div>
+    </div>`
+
+    return cityCard
+}
+
+function renderSearchHistory() {
+    $('#search-history-list').empty()
+    let cityList = getCityListFromLS()
+
+    for (const city of cityList) {
+        let cityCard = createCityListCard(city)
+        $('#search-history-list').append(cityCard)
+    }
+
+    $('.search-history-card').on('click', function(event) {
+        getCityWeather($(this).data('city'))
     })
 }
 
@@ -72,7 +142,7 @@ function createWeatherForecastCard(weatherForecastData) {
     const wind = weatherForecastData.wind.speed
     
     const card = 
-    `<div class="col">
+    `<div class="col forecast-card mx-2 bg-secondary text-light rounded-3">
         <p>${date}</p>
         <img src="https://openweathermap.org/img/wn/${icon}.png"></img>
         <p>Temp: ${temp}°F</p>
@@ -83,10 +153,24 @@ function createWeatherForecastCard(weatherForecastData) {
     return card
 }
 
-getCityWeather('madison')
+renderSearchHistory()
 // function that runs on the submit event that gets city input from input value 
+function getCityInput(event) {
+    
+}
 
+$(document).ready(function () { 
+    $("#search-button").click(function (event) { 
+        let cityName = $('#search-input').val()
 
+        if (cityName) {
+            getCityWeather(cityName)
+        } else {
+            alert('Please enter a valid city')
+        }
+        $('#search-input').val('')
+    }); 
+});
 // create function that makes weather cards 
     // adds them to html (forecast section)
 
